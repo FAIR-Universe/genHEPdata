a9c3b4cab3448e015869abffccacfb8fbf79eb95
ragansu <ragansu@gmail.com>
2024-03-24 17:17:44 +0100

    changed more file locations

--- scripts/generate_events.sh
+++ scripts/generate_events.sh
@@ -5,7 +5,8 @@ export PYTHIA8=/global/cfs/cdirs/m4287/hep/Pythia8
 
 # define the paths
 export Delphes_Path=/global/cfs/cdirs/m4287/hep/Delphes-3.5.0/
-export WorkDir=/global/cfs/cdirs/m4287/hep/fair_universe_simulation/
+export WorkDir=/global/cfs/cdirs/m4287/hep/fair_universe_simulation/genHEPdata/
+export OutputDir=/global/cfs/cdirs/m4287/hep/Delphes_PYTHIA8_output/
 
 cd $Delphes_Path
 
@@ -13,9 +14,9 @@ cd $Delphes_Path
 SEED=$1
 PROCESS=$2
 rm -rf $WorkDir/pythia_$PROCESS_$SEED.root
-echo "Random:seed = $SEED" | cat - $WorkDir/config_$PROCESS.cmnd > $WorkDir/configBkp/config_$PROCESS-$SEED.cmnd
-head $WorkDir/configBkp/config_$PROCESS-$SEED.cmnd
-./DelphesPythia8 $WorkDir/cards/delphes_card_ATLAS_updated.tcl $WorkDir/configBkp/config_$PROCESS-$SEED.cmnd $WorkDir/pythia_$PROCESS'_'$SEED.root
+echo "Random:seed = $SEED" | cat - $WorkDir/share/config_$PROCESS.cmnd > $OutputDir/configBkp/config_$PROCESS-$SEED.cmnd
+head $OutputDir/configBkp/config_$PROCESS-$SEED.cmnd
+./DelphesPythia8 $WorkDir/cards/delphes_card_ATLAS_updated.tcl $OutputDir/configBkp/config_$PROCESS-$SEED.cmnd $OutputDir/root_files/pythia_$PROCESS'_'$SEED.root
 
 
 echo "Waiting for processes to finish...."
