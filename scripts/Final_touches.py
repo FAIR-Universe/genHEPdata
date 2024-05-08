bef5c4dd8110d0a18387e7d23245fb310404918e
ragansu <ragansu@gmail.com>
2024-05-08 15:16:28 -0700

    changes to Final touches

--- scripts/Final_touches.py
+++ scripts/Final_touches.py
@@ -6,17 +6,21 @@ import warnings
 import json
 
 warnings.filterwarnings("ignore")
-from pathlib import Path
-
-Systematics_path = os.path.join(os.getcwd(), "HEP-Challenge", "ingestion_program")
-
-Path.append(Systematics_path)
-from systematics import Systematics, DER_data, Reweight
-from config import LHC_NUMBERS, CSV, PARQUET
+import sys
 
 root_dir = os.path.dirname(os.path.realpath(__file__))
 parent_dir = os.path.dirname(root_dir)
-write_dir = os.path.join(parent_dir, "Datagenerator_reduced_weight")
+write_dir = os.path.join(root_dir, "Data_HEP")
+
+systematics_path = os.path.join(parent_dir, "HEP-Challenge", "ingestion_program")
+
+sys.path.append(systematics_path)
+from systematics import Systematics, DER_data, Reweight
+from config import LHC_NUMBERS, CSV, PARQUET
+
+import argparse
+
+
 
 
 # Load the CSV file
@@ -64,7 +68,6 @@ def from_parquet(data, file_read_loc):
 
 def train_test_data_generator(full_data, verbose=0):
 
-    print("root - dir", root_dir)
 
     # Remove the "label" and "weights" columns from the data
     full_data = clean_data(full_data)
@@ -94,7 +97,9 @@ def train_test_data_generator(full_data, verbose=0):
     return train_set, test_set
 
 
-def dataGenerator(verbose=0):
+def dataGenerator(input_file_loc=os.path.join(root_dir, "input_data"),
+                  output_file_loc=write_dir,
+                  verbose=0):
 
     full_data = {
         "Z": pd.DataFrame(),
@@ -106,11 +111,11 @@ def dataGenerator(verbose=0):
 
     if CSV:
         # Load the data from the CSV files
-        from_csv(full_data, os.path.join(root_dir, "input_data"))
+        from_csv(full_data,input_file_loc)
 
     if PARQUET:
         # Load the data from the parquet files
-        from_parquet(full_data, os.path.join(root_dir, "input_data"))
+        from_parquet(full_data,input_file_loc)
 
     train_set, test_set = train_test_data_generator(full_data, verbose=verbose)
 
@@ -133,19 +138,19 @@ def dataGenerator(verbose=0):
         print(f"[*] --- sum of background : {np.sum(train_weights[train_label==0])}")
 
     # Create directories to store the label and weight files
-    train_label_path = os.path.join(write_dir, "input_data", "train", "labels")
+    train_label_path = os.path.join(output_file_loc, "input_data", "train", "labels")
     if not os.path.exists(train_label_path):
         os.makedirs(train_label_path)
 
-    train_weights_path = os.path.join(write_dir, "input_data", "train", "weights")
+    train_weights_path = os.path.join(output_file_loc, "input_data", "train", "weights")
     if not os.path.exists(train_weights_path):
         os.makedirs(train_weights_path)
 
-    train_data_path = os.path.join(write_dir, "input_data", "train", "data")
+    train_data_path = os.path.join(output_file_loc, "input_data", "train", "data")
     if not os.path.exists(train_data_path):
         os.makedirs(train_data_path)
 
-    train_settings_path = os.path.join(write_dir, "input_data", "train", "settings")
+    train_settings_path = os.path.join(output_file_loc, "input_data", "train", "settings")
     if not os.path.exists(train_settings_path):
         os.makedirs(train_settings_path)
 
@@ -171,15 +176,15 @@ def dataGenerator(verbose=0):
     np.savetxt(os.path.join(train_weights_path), train_weights, fmt="%f")
 
     # Create directories to store the label and weight files
-    reference_settings_path = os.path.join(write_dir, "reference_data", "settings")
+    reference_settings_path = os.path.join(output_file_loc, "reference_data", "settings")
     if not os.path.exists(reference_settings_path):
         os.makedirs(reference_settings_path)
 
-    test_data_path = os.path.join(write_dir, "input_data", "test", "data")
+    test_data_path = os.path.join(output_file_loc, "input_data", "test", "data")
     if not os.path.exists(test_data_path):
         os.makedirs(test_data_path)
 
-    test_settings_path = os.path.join(write_dir, "input_data", "test", "settings")
+    test_settings_path = os.path.join(output_file_loc, "input_data", "test", "settings")
     if not os.path.exists(test_settings_path):
         os.makedirs(test_settings_path)
 
@@ -243,4 +248,17 @@ def dataSimulator(n=1000, verbose=0, tes=1.0, mu=1.0):
 
 
 if __name__ == "__main__":
-    dataGenerator(verbose=2)
+    parser = argparse.ArgumentParser(
+        description="This is script to generate data for the HEP competition."
+    )
+    parser.add_argument("--input", "-i", help="Input file location")
+    parser.add_argument("--output", "-o", help="Output file location")
+    
+    args = parser.parse_args()
+    
+    print("root - dir", root_dir)
+    print("parent - dir", parent_dir)
+
+
+    
+    dataGenerator(args.input,args.output)
