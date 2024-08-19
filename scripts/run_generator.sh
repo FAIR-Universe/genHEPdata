adf79bd509d4b3e68aad464418de65e7ed208667
ragansu <ragansu@gmail.com>
2024-08-19 11:38:58 -0700

    updated job launch mechanism

---
+++ scripts/run_generator.sh
@@ -0,0 +1,17 @@
+##!/bin/bash
+
+export OutputDir=/global/cfs/cdirs/m4287/hep/Delphes_PYTHIA8_output
+export WorkDir=/global/cfs/cdirs/m4287/hep/genHEPdata
+
+shifter --image=rootproject/root:6.28.04-ubuntu22.04 /bin/bash $WorkDir/scripts/generate_events.sh $1 $2
+cd /global/cfs/cdirs/m4287/hep/Delphes-3.5.0
+csv_files=$OutputDir/csv_files_$2
+
+if [ ! -d $csv_files ]; then
+    mkdir $csv_files
+fi
+
+shifter --image=rootproject/root:6.28.04-ubuntu22.04 root -l $WorkDir/scripts/preProcess.cpp\(\"$OutputDir/root_files/pythia_$2"_"$1.root\",\"$csv_files/hist_$2"_"$1\",$3\)
+
+rm -rf $OutputDir/root_files/pythia_$2"_"$1.root
+echo "Done at $(date)"
