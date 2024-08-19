c85ad9079a0839816472d91d9404375e368256f7
ragansu <ragansu@gmail.com>
2024-08-19 12:08:26 -0700

    more process + updated image

--- scripts/run_batch.sh
+++ scripts/run_batch.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --image=rootproject/root:latest
+#SBATCH --image=rootproject/root:6.28.04-ubuntu22.04 
 #SBATCH --account=m4287
 #SBATCH --qos=regular
-#SBATCH -N 1
+#SBATCH -N 4
 #SBATCH --constraint=cpu
 #SBATCH -t 4:00:00
 #SBATCH -J pythia_gen
@@ -12,7 +12,7 @@ process=$2
 label=$3
 export WorkDir=/global/cfs/cdirs/m4287/hep/genHEPdata
 
-srun -n 128 -c 2 bash -c "
-    seed=\$((${main_seed}*100 + \$SLURM_PROCID))
+srun -n 512 -c 2 bash -c "
+    seed=\$((${main_seed}*512 + \$SLURM_PROCID))
     $WorkDir/scripts/run_generator.sh \${seed} ${process} ${label}
 "
--- scripts/run_generator.sh
+++ scripts/run_generator.sh
@@ -1,9 +1,9 @@
 ##!/bin/bash
 
-export OutputDir=/global/cfs/cdirs/m4287/hep/Delphes_PYTHIA8_output
+export OutputDir=/global/cfs/cdirs/m4287/hep/NEW_DelphesPythia_data
 export WorkDir=/global/cfs/cdirs/m4287/hep/genHEPdata
 
-shifter --image=rootproject/root:6.28.04-ubuntu22.04 /bin/bash $WorkDir/scripts/generate_events.sh $1 $2
+sh $WorkDir/scripts/generate_events.sh $1 $2
 cd /global/cfs/cdirs/m4287/hep/Delphes-3.5.0
 csv_files=$OutputDir/csv_files_$2
 
@@ -11,7 +11,7 @@ if [ ! -d $csv_files ]; then
     mkdir $csv_files
 fi
 
-shifter --image=rootproject/root:6.28.04-ubuntu22.04 root -l $WorkDir/scripts/preProcess.cpp\(\"$OutputDir/root_files/pythia_$2"_"$1.root\",\"$csv_files/hist_$2"_"$1\",$3\)
+root -l -b -q  $WorkDir/scripts/preProcess.cpp\(\"$OutputDir/root_files/pythia_$2"_"$1.root\",\"$csv_files/hist_$2"_"$1\",$3\)
 
 rm -rf $OutputDir/root_files/pythia_$2"_"$1.root
 echo "Done at $(date)"
