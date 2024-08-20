186587818e96aac708eb8813366a9d06c7022981
ragansu <ragansu@gmail.com>
2024-08-20 09:00:43 -0700

    fixed issue with shifter

--- scripts/run_batch.sh
+++ scripts/run_batch.sh
@@ -14,7 +14,7 @@ process=$2
 label=$3
 export WorkDir=/global/cfs/cdirs/m4287/hep/genHEPdata
 
-srun -n 64 -c 4 rootproject/root:6.28.04-ubuntu22.04 bash -c "
+srun -n 64 -c 4 shifter bash -c "
     seed=\$((${main_seed}*64 + \$SLURM_PROCID))
     $WorkDir/scripts/run_generator.sh \${seed} ${process} ${label}
 "
